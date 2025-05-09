5b29f4df24c63106daf3362d3e2aae2528bf498c
Hakan Gülmek <gulmekhakan6@gmail.com>
2025-05-09 21:00:13 +0300

    feat: update UserListItem component to include user removal functionality and improve button interaction

--- src/app/component/userListItem.tsx
+++ src/app/component/userListItem.tsx
@@ -2,17 +2,30 @@ import React from "react";
 import ExpandablePanel from "./expandablePanel";
 import AlbumList from "./albumList";
 import { GoTrash } from "react-icons/go";
+import { useRemoveUserMutation } from "../store";
+import CircularProgress from "@mui/material/CircularProgress";
 
 interface User {
   id: number;
   name: string;
 }
 
-function userListItem({ user }: { user: User }) {
+function UserListItem({ user }: { user: User }) {
+  const [removeUser, result] = useRemoveUserMutation();
+  const handleRemoveClick = () => {
+    removeUser(user);
+  };
   const headar = (
     <>
-      <button style={{ marginRight: "10px", border: "none" }}>
-        <GoTrash size={20} color="red" />
+      <button
+        style={{ marginRight: "10px", border: "none", cursor: "pointer" }}
+        onClick={handleRemoveClick}
+      >
+        {result.isLoading ? (
+          <CircularProgress size={20} />
+        ) : (
+          <GoTrash size={20} color="red" />
+        )}
       </button>
       {user.name}
     </>
@@ -26,4 +39,4 @@ function userListItem({ user }: { user: User }) {
   );
 }
 
-export default userListItem;
+export default UserListItem;
