fa6c9b760132a15168523b017eabf7612f0d0141
Hakan Gülmek <gulmekhakan6@gmail.com>
2025-05-10 15:03:58 +0300

    feat: refactor ExpandablePanel component to improve toggle functionality and enhance icon interaction

--- src/app/component/expandablePanel.tsx
+++ src/app/component/expandablePanel.tsx
@@ -1,24 +1,40 @@
+"use client";
 import React from "react";
-import { GoChevronLeft } from "react-icons/go";
+import { GoChevronLeft, GoChevronRight } from "react-icons/go";
+import { useState } from "react";
 
-function expandablePanel({
+function ExpandablePanel({
   header,
   children,
 }: {
   header: React.ReactNode;
   children: React.ReactNode;
 }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleOpenClick = () => {
+    setIsOpen((prev) => !prev);
+  };
   return (
     <div className="panelTop">
       <div className="topArrangement">
         <div className="topArrangement">{header}</div>
-        <div>
-          <GoChevronLeft size={20} color="red" />
+        <div onClick={handleOpenClick}>
+          {isOpen ? (
+            <GoChevronRight size={20} style={{ transform: "rotate(90deg)" }} />
+          ) : (
+            <GoChevronLeft size={20} />
+          )}
         </div>
       </div>
-      <div className="content">{children}</div>
+      <div className="content">
+        {isOpen && (
+          <div className="contentArrangement">
+            <div className="contentArrangement">{children}</div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
 
-export default expandablePanel;
+export default ExpandablePanel;
